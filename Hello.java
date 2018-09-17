faa0dc85f8eda9ae04c4675f90f8518b38d8cf95
Kevin3719 <36079138+Kevin3719@users.noreply.github.com>
2018-09-17 14:08:08 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 
-public class HelloWorld{
+public class Hello{
     public static void main(String[] args){
         System.out.println("Hello my name is Kevin Chen, I like to eat popcorn chicken and cookies.");
     }
